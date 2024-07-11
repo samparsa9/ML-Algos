d3a4cdb26e4447b52d1768585880f86f5469a11b
malikkurtz <malikkurtz129@gmail.com>
2024-07-11 00:27:37 -0400

    finished linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,53 +1,83 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-#x = np.linspace(0, 10, 100)
-data = [[0.8, 1.5, 2.3, 3.4, 4.4, 5.2, 5.4, 5.7, 6.2],[1.2, 1.5, 2.5, 3.1, 3.3, 3.8, 4.8, 5.5, 6.0]]
-#y = np.array([1.2, 1.5, 2.5, 3.1, 3.3, 3.8, 4.8, 5.5, 6.0])
-#y = 2 * x + np.random.normal(0, 1, 100)
+import sympy as sp
+import scipy as sc
+from scipy import optimize
+from scipy.misc import derivative
+from findiff import FinDiff
+import time as tm
 
-data = np.array(data)
-
-def LinearRegression(training_data, num_features=1): # num_features will specify the shape of the input data
+class LinearRegression: # num_features will specify the shape of the input data
+    def __init__(self, num_features=1, learning_rate=0.01, w=-0, b=0):
+        self.learning_rate = learning_rate
+        self.w = w
+        self.b = b
+        self.x = None
+        self.y = None
+        self.predictions = None
     #data is a list of dimensions which has a vector each
     #ex) x_train and y_train are x and y values that correspond with each other, both in the 'data' list    
 
     #the last list in data will be the label vector (what we are predicting)
     #any vectors preceding will
-    x_train = np.array(training_data[0])
-    y_train = np.array(training_data[-1])
-    #print(xpoints)
-    #print(ypoints)
-    
-    #STEPS:
-    #1) Plot Data
-
-    plt.plot(x_train, y_train, 'o')
-    plt.xlim(0, 7)
-    plt.ylim(0, 7)
     
 
-    w = 0
-    b = 3
-    def fit_line(x_train, w, b):
-        y_hat = np.multiply(x_train, w)
-        y_hat = np.add(y_hat,b)
-        return y_hat
+    # Function/Equation for a line
+    def f(self,x, w, b): 
+        return (x * w) + b
+
+    # This is our cost function that we need to take partial derivates of parameters with respect to
+    def loss_function(self, x, y, w, b):
+        y_hat = self.f(x, w, b) # y_hat is our predictions, it produces our y values to plot our line of best fit
+        return np.mean((y - y_hat) ** 2)
+
+
+    # While either of our partial derivatives are greater than 0.01, keep running gradient descent
+    def fit(self,x,y):
+        self.x = x
+        self.y = y
+        h = 1e-5
+        optimized = False
+        while not optimized:
+            dw = (self.loss_function(x,y,self.w+h,self.b) - self.loss_function(self.x,self.y, self.w-h, self.b)) / (2*h)
+            print(f"New dw: {dw}")
+            tm.sleep(0.1)
+            db = (self.loss_function(x,y,self.w,self.b+h ) - self.loss_function(x,y,self.w, self.b-h)) / (2*h)
+            print(f"New db: {db}")
+            tm.sleep(0.1)
+            temp_w = self.w + (-1*(self.learning_rate * dw))
+            temp_b = self.b + (-1*(self.learning_rate * db))
+            self.w = temp_w
+            # print(f"new w: {self.w}")
+            self.b = temp_b
+            # print(f"new b: {self.b}")
+            if abs(dw) < 0.1 and abs(db) < 0.1:
+                optimized = True
     
-    y_hat = fit_line(x_train, w, b)
-    print(y_hat)
-    print(y_hat.shape)
-    plt.plot(x_train, y_hat, '')
+        self.predictions = self.f(x, self.w, self.b)
+
+
     
+    def plot(self):
+        plt.plot(self.x, self.y, 'o')
+        plt.plot(self.x, self.predictions)
+        #plt.xlim(0, 7)
+        #plt.ylim(0, 7)
+        plt.show()
 
-    plt.show()
-    
-    return (w,b)
 
+data = [[0.8, 1.5, 2.3, 3.4, 4.4, 5.2, 5.4, 5.7, 6.2],[1.2, 1.5, 2.5, 3.1, 3.3, 3.8, 4.8, 5.5, 6.0]]
+#y = np.array([1.2, 1.5, 2.5, 3.1, 3.3, 3.8, 4.8, 5.5, 6.0])
+#y = 2 * x + np.random.normal(0, 1, 100)
 
+data = np.array(data)
+model = LinearRegression()
+model.fit(data[0], data[1])
+print(model.y)
+print(model.predictions)
+model.plot()
 
-
-LinearRegression(data)
 #print(LinearRegression(data))
 
     
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+w = 0
+b = 3
+w, b = np.mgrid[-100:101:25., -100:101:25.]
+
+print(w)
+print(b)
